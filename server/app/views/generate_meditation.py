c674b011cb8c7e3f547e83cf52265372a0d7a9b3
Sebastian Wakefield <seb7wake@gmail.com>
2023-09-16 17:35:54 -0400

    adding challenge view

--- server/app/views/generate_meditation.py
+++ server/app/views/generate_meditation.py
@@ -9,50 +9,59 @@ import nltk
 import numpy as np
 import time
 
+
 class GenerateMeditationView(APIView):
     def get(self, request, format=None):
         meditation = self.generate_meditations(request.data)
         # self.google_cloud_tts(meditation)
-        self.bark_tts( meditation)
+        self.bark_tts(meditation)
         return Response(meditation)
-    
+
     def generate_meditations(self, data):
         if not data:
-            data = {"duration": 1, "context": "I have a big presentation coming up and I'm feeling nervous."}
+            data = {
+                "duration": 1,
+                "context": "I have a big presentation coming up and I'm feeling nervous.",
+            }
         prompt = f"Generate a unique personalized guided meditation in no more than {str(data['duration']*6)} sentences, please.\n\n"
         if data.get("duration"):
-            prompt += "Duration: " + str(data['duration']) + " minutes" + "\n"
+            prompt += "Duration: " + str(data["duration"]) + " minutes" + "\n"
         if data.get("technique"):
             prompt += "Meditation Technique: " + data.get("technique") + "\n"
         if data.get("context"):
             prompt += "Situation: " + data.get("context") + "\n"
         prompt += f"\nPlease create a tailored guided meditation that aligns directly with the current state and situation, {', technique, and duration' if data.get('technique') else 'and duration' }. Make it as personalized as possible. Thank you!"
-        
-        print('sending request...')
+
+        print("sending request...")
         print("prompt:", prompt, "\n\n")
         headers = {
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + os.environ.get("OPENAI_API_KEY")
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + os.environ.get("OPENAI_API_KEY"),
         }
         try:
-            res = requests.post(url="https://api.openai.com/v1/chat/completions", headers=headers, json=self.get_json(prompt)).json()["choices"][0]['message']['content']
+            res = requests.post(
+                url="https://api.openai.com/v1/chat/completions",
+                headers=headers,
+                json=self.get_json(prompt),
+            ).json()["choices"][0]["message"]["content"]
         except KeyError as e:
             print("error:", e)
-            return {"error": "Sorry, I couldn't generate a meditation for you. Please try again."}
+            return {
+                "error": "Sorry, I couldn't generate a meditation for you. Please try again."
+            }
         return res
-        
-    
+
     def get_json(self, prompt):
         return {
             "model": "gpt-3.5-turbo",
             "messages": [{"role": "user", "content": prompt}],
-            "temperature": 1.2
+            "temperature": 1.2,
         }
 
     def google_cloud_tts(self, meditation):
         print("meditation result:", meditation)
         meditation = meditation.split("\n\n")
-        text = '<speak>'
+        text = "<speak>"
         for i in range(len(meditation)):
             if meditation[i]:
                 text += meditation[i]
@@ -68,7 +77,8 @@ class GenerateMeditationView(APIView):
         # Build the voice request, select the language code ("en-US") and the ssml
         # voice gender ("neutral")
         voice = texttospeech.VoiceSelectionParams(
-            name="en-GB-Neural2-D", language_code="en-GB", 
+            name="en-GB-Neural2-D",
+            language_code="en-GB",
         )
 
         # Select the type of audio file you want returned
@@ -87,19 +97,21 @@ class GenerateMeditationView(APIView):
             out.write(response.audio_content)
             time.sleep(5)
             print('Audio content written to file "meditation.mp3"')
-        
+
     def bark_tts(self, meditation):
         # download and load all models
         preload_models()
-        nltk.download('punkt')
+        nltk.download("punkt")
         # generate audio from text
         #  "As you exhale, let go of any tension or worry in your body. Feel yourself becoming more relaxed with each breath."
-        text_prompt = "Close your eyes and take a deep breath. Imagine yourself standing confidently in front of your audience, feeling calm and composed." +\
-        "Visualize each step of your presentation, from the opening remarks to your final slide." +\
-        "Feel an overwhelming sense of confidence and know that you are well-prepared." +\
-        "Allow any nervousness or anxiety to melt away as you focus your mind on your knowledge and expertise. " +\
-        "Repeat the mantra, \"I am prepared, I am confident, I am capable.\". " +\
-        "Open your eyes, feeling rejuvenated and ready to tackle your presentation with ease and grace."
+        text_prompt = (
+            "Close your eyes and take a deep breath. Imagine yourself standing confidently in front of your audience, feeling calm and composed."
+            + "Visualize each step of your presentation, from the opening remarks to your final slide."
+            + "Feel an overwhelming sense of confidence and know that you are well-prepared."
+            + "Allow any nervousness or anxiety to melt away as you focus your mind on your knowledge and expertise. "
+            + 'Repeat the mantra, "I am prepared, I am confident, I am capable.". '
+            + "Open your eyes, feeling rejuvenated and ready to tackle your presentation with ease and grace."
+        )
         sentences = nltk.sent_tokenize(text_prompt)
         silence = np.zeros(int(5 * SAMPLE_RATE))
         last_silence = np.zeros(int(10 * SAMPLE_RATE))
@@ -116,6 +128,3 @@ class GenerateMeditationView(APIView):
 
         # play text in notebook
         write_wav("../mobile/assets/meditation_speaker_3.wav", SAMPLE_RATE, audio)
-
-
-        
