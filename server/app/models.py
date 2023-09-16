8eb4c40ce91017768ea2e16041c2d41fc5e9ee4a
Sebastian Wakefield <seb7wake@gmail.com>
2023-09-16 01:27:51 -0400

    updating initial models

--- server/app/models.py
+++ server/app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Challenge(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, related_name='challenges', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+class Journal(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    feeling = models.IntegerField()
+    user = models.ForeignKey(User, related_name='journals', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
