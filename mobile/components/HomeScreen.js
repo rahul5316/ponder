c684bf0d564a1cc2e527ff0d6259b30843085ed9
rsinha01 <rsinha01@students.cs.ubc.ca>
2023-09-16 19:09:58 -0400

    Added Cards on the homescreen

--- mobile/components/HomeScreen.js
+++ mobile/components/HomeScreen.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useNavigation } from "@react-navigation/native";
 
+function Card({ title, description, buttonText, onPress }) {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.cardTitle}>{title}</Text>
+      <Text style={styles.cardDescription}>{description}</Text>
+      <TouchableOpacity style={styles.cardButton} onPress={onPress}>
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
 function HomeScreen() {
+  const navigation = useNavigation();
+  
   return (
     <View style={styles.page}>
-      <Text>Home Screen</Text>
+      <Card 
+        title="Guided Meditation"
+        description={`How are you feeling today, [Name]?`}
+        buttonText="Check in"
+        onPress={() => navigation.navigate('GuidedMeditation')}
+      />
+      <Card 
+        title="Weekly Challenges"
+        description={`Self-Compassion Practice\n\nWrite a letter of self-compassion to yourself, acknowledging your strengths and forgiving your imperfections.`}
+        buttonText="View"
+        onPress={() => navigation.navigate('WeeklyChallenges')}
+      />
+      <Card 
+        title="Journal"
+      />
     </View>
   );
 }
@@ -68,8 +96,31 @@ export default function App() {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 20,
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 15,
+  },
+  cardTitle: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  cardDescription: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  cardButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
   },
   homeIcon: {
     width: 29,
