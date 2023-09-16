0ecf465c902a97cc285c59ec9bbfe336166b2d52
rsinha01 <rsinha01@students.cs.ubc.ca>
2023-09-16 05:51:20 -0400

    Audio added

--- mobile/App.js
+++ mobile/App.js
@@ -5,7 +5,6 @@ import MeditationTimer from './components/MeditationTimer';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <MeditationTimer />
     </View>
--- mobile/components/MeditationTimer.js
+++ mobile/components/MeditationTimer.js
@@ -1,16 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import { Circle, Svg } from 'react-native-svg';
+import { Audio } from 'expo-av';
+import { Asset } from 'expo-asset';
 
 const MeditationTimer = () => {
     const [isActive, setIsActive] = useState(false);
-    const [progress, setProgress] = useState(0);  // 0 to 1
+    const [progress, setProgress] = useState(0);
+    const [sound, setSound] = useState();
+
+    useEffect(() => {
+      async function setupAudio() {
+        try {
+          await Audio.setAudioModeAsync({
+            playsInSilentModeIOS: true,
+            staysActiveInBackground: true,
+            shouldDuckAndroid: true,
+          });
+      
+          const soundInstance = new Audio.Sound();
+          const soundAsset = Asset.fromModule(require('../assets/output2.wav'));
+          await soundInstance.loadAsync(soundAsset);
+          setSound(soundInstance);
+          await soundInstance.playAsync();
+        } catch (error) {
+          console.error("Error setting up audio:", error);
+        }
+      }
+      setupAudio();
+    }, []);
+
+    async function playSound() {
+        try {
+            await sound.replayAsync();
+        } catch (error) {
+            console.error("Error playing sound:", error);
+        }
+    }
+
+    const handlePress = async () => {
+        if (isActive) {
+            sound && await sound.pauseAsync();  
+        } else {
+            sound && await sound.playAsync(); 
+        }
+        setIsActive(!isActive);
+    }
 
     useEffect(() => {
         let interval;
         if (isActive && progress < 1) {
             interval = setInterval(() => {
-                setProgress(prevProgress => prevProgress + 0.01);
+                setProgress(prevProgress => prevProgress + 0.01); 
             }, 1000);
         } else {
             clearInterval(interval);
@@ -34,7 +75,7 @@ const MeditationTimer = () => {
             </Svg>
             <Button
                 title={isActive ? 'Pause' : 'Start'}
-                onPress={() => setIsActive(!isActive)}
+                onPress={handlePress}
                 style={styles.button}
             />
         </View>
