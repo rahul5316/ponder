2334891d2fd5a098bde509029122992c8a6d57cc
Sebastian Wakefield <seb7wake@gmail.com>
2023-09-16 14:26:57 -0400

    updating meditation text to speech process

--- server/app/views/generate_meditation.py
+++ server/app/views/generate_meditation.py
@@ -7,6 +7,7 @@ from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
 import nltk
 import numpy as np
+import time
 
 class GenerateMeditationView(APIView):
     def get(self, request, format=None):
@@ -16,16 +17,19 @@ class GenerateMeditationView(APIView):
         return Response(meditation)
     
     def generate_meditations(self, data):
-        prompt = "Generate a personalized guided meditation for me, please.\n\n"
+        if not data:
+            data = {"duration": 1, "context": "I have a big presentation coming up and I'm feeling nervous."}
+        prompt = f"Generate a unique personalized guided meditation in no more than {str(data['duration']*6)} sentences, please.\n\n"
         if data.get("duration"):
-            prompt += "Duration: " + data.get("duration") + "\n"
+            prompt += "Duration: " + str(data['duration']) + " minutes" + "\n"
         if data.get("technique"):
             prompt += "Meditation Technique: " + data.get("technique") + "\n"
         if data.get("context"):
             prompt += "Situation: " + data.get("context") + "\n"
-        prompt += "\nPlease create a tailored guided meditation that aligns with my current state and situation using the chosen meditation technique. Make it as personalized as possible. Thank you!"
+        prompt += f"\nPlease create a tailored guided meditation that aligns directly with the current state and situation, {', technique, and duration' if data.get('technique') else 'and duration' }. Make it as personalized as possible. Thank you!"
         
         print('sending request...')
+        print("prompt:", prompt, "\n\n")
         headers = {
             'Content-Type': 'application/json',
             "Authorization": "Bearer " + os.environ.get("OPENAI_API_KEY")
@@ -46,19 +50,25 @@ class GenerateMeditationView(APIView):
         }
 
     def google_cloud_tts(self, meditation):
-        print("creating audio for meditation:", meditation)
+        print("meditation result:", meditation)
+        meditation = meditation.split("\n\n")
+        text = '<speak>'
+        for i in range(len(meditation)):
+            if meditation[i]:
+                text += meditation[i]
+                text += '<break time="3s"/>' if i != len(meditation) - 1 else ""
+        text += "</speak>"
+
+        print("creating meditation text:", text)
         # Instantiates a client
         client = texttospeech.TextToSpeechClient()
 
-        text_prompt = "Close your eyes and take a deep breath in, filling your lungs with fresh air." + \
-        "As you exhale, let go of any tension or worry in your body. Feel yourself becoming more relaxed with each breath."
-        # Set the text input to be synthesized
-        synthesis_input = texttospeech.SynthesisInput(text=text_prompt)
+        synthesis_input = texttospeech.SynthesisInput(ssml=text)
 
         # Build the voice request, select the language code ("en-US") and the ssml
         # voice gender ("neutral")
         voice = texttospeech.VoiceSelectionParams(
-            name="en-GB-Neural2-D", language_code="en-GB"
+            name="en-GB-Neural2-D", language_code="en-GB", 
         )
 
         # Select the type of audio file you want returned
@@ -68,47 +78,44 @@ class GenerateMeditationView(APIView):
             # pitch=0.95,
         )
 
-        # Perform the text-to-speech request on the text input with the selected
-        # voice parameters and audio file type
-        response = client.synthesize_speech(
-            input=synthesis_input, voice=voice, audio_config=audio_config
-        )
-
         # The response's audio_content is binary.
-        with open("../mobile/assets/meditation.mp3", "wb") as out:
+        with open(f"../mobile/assets/meditation.mp3", "wb") as out:
+            response = client.synthesize_speech(
+                input=synthesis_input, voice=voice, audio_config=audio_config
+            )
             # Write the response to the output file.
             out.write(response.audio_content)
-            print('Audio content written to file "output.mp3"')
-    
+            time.sleep(5)
+            print('Audio content written to file "meditation.mp3"')
+        
     def bark_tts(self, meditation):
         # download and load all models
         preload_models()
         nltk.download('punkt')
         # generate audio from text
-        text_prompt = "Close your eyes and take a deep breath in, filling your lungs with fresh air." + \
-            "As you exhale, let go of any tension or worry in your body. Feel yourself becoming more relaxed with each breath." + \
-            "Visualize a peaceful, serene place in your mind. It could be a beach, a forest, or any place where you feel calm and safe." + \
-            "Imagine yourself standing in this peaceful place, feeling a sense of tranquility and confidence." + \
-            "Now, bring your focus to your upcoming project presentation. Acknowledge the worry and anxiety you're feeling about it." + \
-            "Take a moment to explore why you're feeling worried. Is it fear of failure, judgment, or the unknown?" + \
-            "Remind yourself that it's normal to feel nervous before a big presentation. You are capable and well-prepared." + \
-            "Envision a bubble of calm and confidence surrounding you. See it growing stronger with each breath you take." + \
-            "Visualize yourself giving the presentation with ease and confidence. Imagine the audience engaged and supportive." + \
-            "Take another deep breath and as you exhale, release any remaining tension or doubt. You are ready for this." + \
-            "Now, gently open your eyes and return to the present moment with a sense of calm and confidence." + \
-            "Carry this feeling of inner peace and assurance with you as you prepare for your presentation."
+        #  "As you exhale, let go of any tension or worry in your body. Feel yourself becoming more relaxed with each breath."
+        text_prompt = "Close your eyes and take a deep breath. Imagine yourself standing confidently in front of your audience, feeling calm and composed." +\
+        "Visualize each step of your presentation, from the opening remarks to your final slide." +\
+        "Feel an overwhelming sense of confidence and know that you are well-prepared." +\
+        "Allow any nervousness or anxiety to melt away as you focus your mind on your knowledge and expertise. " +\
+        "Repeat the mantra, \"I am prepared, I am confident, I am capable.\". " +\
+        "Open your eyes, feeling rejuvenated and ready to tackle your presentation with ease and grace."
         sentences = nltk.sent_tokenize(text_prompt)
-        silence = np.zeros(int(2 * SAMPLE_RATE))
+        silence = np.zeros(int(5 * SAMPLE_RATE))
+        last_silence = np.zeros(int(10 * SAMPLE_RATE))
         speaker = "v2/en_speaker_3"
         pieces = []
-        for sentence in sentences:
+        for i, sentence in enumerate(sentences):
             audio_array = generate_audio(sentence, history_prompt=speaker)
-            pieces += [audio_array, silence.copy()]
+            if i == len(sentences) - 2:
+                pieces += [audio_array, last_silence.copy()]
+            else:
+                pieces += [audio_array, silence.copy()]
         audio = np.concatenate(pieces)
         # speech_array = generate_audio(text_prompt, history_prompt=speaker)
 
         # play text in notebook
-        write_wav("../meditation_speaker_3.wav", SAMPLE_RATE, audio)
+        write_wav("../mobile/assets/meditation_speaker_3.wav", SAMPLE_RATE, audio)
 
 
         
