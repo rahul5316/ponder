167f5c93c1f7d48b3109da77c229b29443ae590b
Sebastian Wakefield <seb7wake@gmail.com>
2023-09-17 01:26:22 -0400

    fixing issue

--- mobile/api/meditation.js
+++ mobile/api/meditation.js
@@ -2,6 +2,5 @@ export const createMeditation = async (data) => {
   let response = await fetch(
     `${process.env.EXPO_PUBLIC_SERVER_URL}/api/meditation`
   );
-  const data = response.status === 200 ? await response.json() : [];
-  return { status: response.status, data };
+  return { status: response.status };
 };
