e32422d90e08ac4fd133f1c68170038eb3e048c9
Noah Stasuik <97553023+noahstaz@users.noreply.github.com>
2023-09-17 04:05:26 -0400

    Updated Duration

--- mobile/components/Duration.js
+++ mobile/components/Duration.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
 const Duration = ({ route, navigation }) => {
@@ -16,18 +16,27 @@ const Duration = ({ route, navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Picker
-        selectedValue={selectedDuration}
-        onValueChange={(itemValue) => setSelectedDuration(itemValue)}
-        style={styles.pickerStyle}
-        mode="dropdown"
-      >
-        {Array.from({ length: 10 }, (_, i) => i + 1).map((min) => (
-          <Picker.Item key={min} label={String(min)} value={min} />
-        ))}
-      </Picker>
-      <Text style={styles.minText}>min</Text>
-      <Button title="Next" onPress={handleNextPress} />
+      <Text style={[styles.title, { color: 'white' }]}>Session Duration</Text>
+      <View style={styles.pickerContainer}>
+        <Picker
+          selectedValue={selectedDuration}
+          onValueChange={(itemValue) => setSelectedDuration(itemValue)}
+          style={styles.pickerStyle}
+          mode="dropdown"
+          itemStyle={styles.pickerItem}
+        >
+          {Array.from({ length: 10 }, (_, i) => i + 1).map((min) => (
+            <Picker.Item key={min} label={String(min)} value={min} />
+          ))}
+        </Picker>
+        <Text style={styles.minText}>min</Text>
+      </View>
+      <TouchableOpacity
+          onPress={() => navigation.navigate('MeditationTimer')}
+          style={styles.buttonContainer}
+        >
+          <Text style={styles.buttonText}>Begin Session</Text>
+        </TouchableOpacity>
     </View>
   );
 };
@@ -39,12 +48,41 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#2A0060",
   },
+  pickerContainer: {
+    flexDirection: "row", // Arrange the Picker and Text horizontally
+    alignItems: "center", // Align items vertically in the center
+  },
   pickerStyle: {
     height: 200,
     width: 100,
   },
   minText: {
     fontSize: 16,
+    color: "#FFFFFF",
+    paddingTop: 15,
+  },
+  pickerItem: {
+    color: "#FFFFFF",
+    backgroundColor: "#2A0060",
+  },
+  buttonContainer: {
+    backgroundColor: '#7000E0',
+    borderRadius: 14,
+    paddingVertical: 15,
+    paddingHorizontal: "20%",
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 20,
   },
 });
 
